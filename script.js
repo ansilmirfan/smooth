254e7dde6805f9176e0d18580027fb93905433fe
ansilmirfan <151526179+ansilmirfan@users.noreply.github.com>
2023-11-29 10:54:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+window.onscroll = function() {
+    var theta = document.documentElement.scrollTop / 50 % Math.PI;
+
+document.getElementById('js-logo').style.transform ='rotate(' + theta + 'rad)';
+}
